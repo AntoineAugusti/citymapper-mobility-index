92fbd96b5b8c9c3810767e3951545eca39a7a439
Antoine Augusti <antoine.augusti@gmail.com>
2020-04-01 11:38:00 -0400

    Build script: push on data.gouv.fr

--- build.sh
+++ build.sh
@@ -47,3 +47,11 @@ wget -O vienna.html https://citymapper.com/cmi/vienna
 
 cd ..
 python main.py
+
+export API="https://www.data.gouv.fr/api/1"
+export DATASET_ID="5e7ca7511bc790fc68223a2f"
+export RESOURCE="0e56f0f4-6a82-48d4-b677-96e950100176"
+curl -H "Accept:application/json" \
+  -H "X-Api-Key:$DATA_GOUV_API_KEY" \
+  -F "file=@data.csv" \
+  -X POST $API/datasets/$DATASET_ID/resources/$RESOURCE/upload/
